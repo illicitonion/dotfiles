051ac05945d9787bc0247f82648e195c6b13a365
Daniel Wagner-Hall <dan@illicitonion.com>
2015-06-04 01:05:50 -0400

    Make vim setup script work on OSX
    Also, make it somewhat more idempotent

--- vim/setup.sh
+++ vim/setup.sh
@@ -1,5 +1,12 @@
-#!/bin/bash
+#!/bin/bash -eu
 
-ln -s $(realpath $0)/vimrc ~/.vimrc
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+self="$0"
+if [[ self != /* ]]; then
+  self="$(pwd)/${self}"
+fi
+
+ln -s "$(dirname ${self})/vimrc" ~/.vimrc
+if [[ ! -e ~/.vim/bundle/Vundle.vim ]]; then
+  git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
 vim +PluginInstall +qall
