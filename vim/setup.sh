da3a113b5e7b9d02aac9947286afc8bbb1537db4
Daniel Wagner-Hall <dawagner@gmail.com>
2015-09-26 00:34:28 +0000

    Install goimports

--- vim/setup.sh
+++ vim/setup.sh
@@ -10,3 +10,5 @@ if [[ ! -e ~/.vim/bundle/Vundle.vim ]]; then
   git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 vim +PluginInstall +qall
+
+go get golang.org/x/tools/cmd/goimports
