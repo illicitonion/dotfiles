94e3a8510f463266bc9ac75dcb035c90dfa353ed
Daniel Wagner-Hall <dawagner@gmail.com>
2016-01-02 18:48:29 +0000

    Only compile realpath if non-linux

--- 00-realpath/setup.sh
+++ 00-realpath/setup.sh
@@ -5,10 +5,11 @@ cd "$(dirname $0)"
 if ! which realpath >/dev/null; then
   if [[ "$(uname)" == "Linux" ]]; then
     sudo apt-get install -y realpath
+  else
+    clang -Wall -Werror -o realpath realpath.c
+    if [[ ! -e ~/bin ]]; then
+      mkdir ~/bin
+    fi
+    ln -s "$(./realpath ./realpath)" ~/bin/realpath
   fi
-  clang -Wall -Werror -o realpath realpath.c
-  if [[ ! -e ~/bin ]]; then
-    mkdir ~/bin
-  fi
-  ln -s "$(./realpath ./realpath)" ~/bin/realpath
 fi
