77124b815a4a6fca34f029863ff62f22e848eb61
Daniel Wagner-Hall <dawagner@gmail.com>
2020-05-20 22:51:54 +0100

    Homebrew

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash -eu
 
+if [[ "$(uname)" == "Darwin" ]]; then
+  (cd brew && ./setup.sh)
+fi
+
 root="$(cd "$(dirname "$0")" && pwd -P)"
 
 for f in "${root}"/*/setup.sh; do
+  echo "${f}"
   (cd "$(dirname "${f}")" && ./setup.sh)
 done
