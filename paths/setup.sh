7b1235ed8715c423f03f6982066fa6b9563e852c
Daniel Wagner-Hall <dawagner@gmail.com>
2023-04-27 08:45:18 +0100

    Add cpabs/cpwd

--- paths/setup.sh
+++ paths/setup.sh
@@ -3,6 +3,8 @@
 . "$(dirname $0)/../common.sh"
 
 symlink abspath ~/bin/abspath
+symlink cpabs ~/bin/cpabs
+symlink cppwd ~/bin/cppwd
 symlink parent_containing_file ~/bin/parent_containing_file
 symlink scppath ~/bin/scppath
 symlink with_isolated_path ~/bin/with_isolated_path
