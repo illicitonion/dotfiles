491b97ffda394ac51a55b3f1c33cdea388d9a326
Daniel Wagner-Hall <dwagnerhall@apple.com>
2023-05-23 14:49:52 +0100

    Add cw, vw

--- paths/setup.sh
+++ paths/setup.sh
@@ -5,6 +5,8 @@
 symlink abspath ~/bin/abspath
 symlink cpabs ~/bin/cpabs
 symlink cppwd ~/bin/cppwd
+symlink cw ~/bin/cw
 symlink parent_containing_file ~/bin/parent_containing_file
 symlink scppath ~/bin/scppath
 symlink with_isolated_path ~/bin/with_isolated_path
+symlink vw ~/bin/vw
