9ecaa48c34101b16896cb338e7b35a3f8bcc3a7d
Daniel Wagner-Hall <dwagnerhall@apple.com>
2023-05-23 13:36:03 +0100

    rust: Remove ios target

--- rust/setup.sh
+++ rust/setup.sh
@@ -3,12 +3,3 @@
 . "$(dirname $0)/../common.sh"
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-
-case "$(uname)" in
-  Darwin)
-    "${HOME}/.cargo/bin/rustup" target add x86_64-apple-ios
-    ;;
-  *)
-    echo >&2 "Unrecognised uname"
-    exit 1
-esac
